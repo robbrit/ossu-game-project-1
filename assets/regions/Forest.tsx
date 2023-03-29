c01bc12efdf8be05438c8e4f5d595d9e88237e01
Rob Britton <rob@robbritton.com>
2023-03-29 10:53:09 -0400

    Some tweaks to the forest tileset.

--- assets/regions/Forest.tsx
+++ assets/regions/Forest.tsx
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Forest" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="forest-tileset.png" width="128" height="128"/>
+<tileset version="1.9" tiledversion="1.9.2" name="Forest" tilewidth="32" tileheight="32" tilecount="32" columns="4">
+ <image source="forest-tileset.png" width="128" height="256"/>
  <wangsets>
   <wangset name="Forest" type="mixed" tile="-1">
    <wangcolor name="Grass" color="#00ff11" tile="-1" probability="1"/>
    <wangcolor name="Tree" color="#007900" tile="-1" probability="1"/>
    <wangcolor name="Dirt" color="#550000" tile="-1" probability="1"/>
-   <wangcolor name="Road" color="#6c6c6c" tile="-1" probability="1"/>
+   <wangcolor name="Wall" color="#6c6c6c" tile="-1" probability="1"/>
    <wangtile tileid="0" wangid="1,1,1,1,1,1,1,1"/>
    <wangtile tileid="1" wangid="2,2,2,2,2,2,2,2"/>
    <wangtile tileid="2" wangid="3,3,3,3,3,3,3,3"/>
@@ -23,6 +23,23 @@
    <wangtile tileid="13" wangid="2,2,2,2,2,2,2,2"/>
    <wangtile tileid="14" wangid="3,3,3,3,3,3,3,3"/>
    <wangtile tileid="15" wangid="4,4,4,4,4,4,4,4"/>
+  </wangset>
+  <wangset name="Forest Corners" type="corner" tile="-1">
+   <wangcolor name="Grass" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Tree" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="Dirt" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="16" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="17" wangid="0,0,0,0,0,0,0,2"/>
+   <wangtile tileid="18" wangid="0,0,0,0,0,0,0,3"/>
+   <wangtile tileid="20" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="21" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="22" wangid="0,0,0,0,0,3,0,0"/>
+   <wangtile tileid="24" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="25" wangid="0,2,0,0,0,0,0,0"/>
+   <wangtile tileid="26" wangid="0,3,0,0,0,0,0,0"/>
+   <wangtile tileid="28" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="29" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="30" wangid="0,0,0,3,0,0,0,0"/>
   </wangset>
  </wangsets>
 </tileset>
