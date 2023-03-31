597d075259dde9efd70bcaba5588af747cc7f6ca
Rob Britton <rob@robbritton.com>
2023-03-31 14:54:14 -0400

    Added a basic NPC.

--- game/regions/region1.py
+++ game/regions/region1.py
@@ -7,3 +7,8 @@ def building1_door(api: scripts.GameAPI) -> None:
 
 def building1_exit(api: scripts.GameAPI) -> None:
     api.change_region("Region1", "Building 1 Exit")
+
+
+class IntroCharacter(scripts.Script, scripts.SavesAPI):
+    def on_activate(self, owner: scripts.ScriptOwner, player: scripts.Player) -> None:
+        print("Welcome!")
