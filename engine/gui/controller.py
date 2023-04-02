05add3dc9baaf10380c59fe40cd7a16908c7f262
Jon Licjon <licjon@protonmail.com>
2023-04-02 15:25:41 -0400

    Implement gui navigation, create load button and start-selected-button, add load button/asset and start-selected-button asset to start-screen.json

--- engine/gui/controller.py
+++ engine/gui/controller.py
@@ -35,6 +35,15 @@ class GuiController:
     def on_key_release(self, symbol: int, modifiers: int) -> None:
         if symbol == arcade.key.ENTER:
             self._activate_current_button()
+        elif symbol == arcade.key.RIGHT and self.selected_button.right is not None:
+            # TODO(jon): current button should show unselected image.
+            self._change_current_button(self.selected_button.right)
+        elif symbol == arcade.key.LEFT and self.selected_button.left is not None:
+            self._change_current_button(self.selected_button.left)
+        elif symbol == arcade.key.UP and self.selected_button.up is not None:
+            self._change_current_button(self.selected_button.up)
+        elif symbol == arcade.key.DOWN and self.selected_button.down is not None:
+            self._change_current_button(self.selected_button.down)
 
         # TODO(rob): Handle activate, cancel, and direction keys.
 
@@ -58,3 +67,8 @@ class GuiController:
             return
 
         self.selected_button.action(self.api)
+
+    def _change_current_button(self, button_name) -> None:
+        for next_button in self.gui.spec.buttons:
+            if button_name == next_button.selected_image_asset:
+                self.selected_button = next_button
