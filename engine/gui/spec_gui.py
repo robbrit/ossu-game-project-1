177f25ad34b25564de3e29568fa2734c98be56be
Rob Britton <rob@robbritton.com>
2023-04-02 16:02:02 -0400

    Refactor so that not all GUIs need a spec.

---
+++ engine/gui/spec_gui.py
@@ -0,0 +1,31 @@
+import arcade
+
+from engine.gui import widgets
+
+
+class SpecGUI:
+    """A GUI class that draws itself based on a spec."""
+
+    sprites: arcade.SpriteList
+
+    def __init__(self, spec: widgets.GUISpec):
+        self.spec = spec
+        self.sprites = arcade.SpriteList()
+        self._load_sprites()
+
+    def draw(self) -> None:
+        self.sprites.draw()
+
+    def _load_sprites(self) -> None:
+        assets = {name: path for name, path in self.spec.assets}
+
+        self.sprites = arcade.SpriteList()
+
+        for button_spec in self.spec.buttons:
+            # TODO(rob): Also load the selected image asset.
+            button = arcade.Sprite(assets[button_spec.unselected_image_asset])
+            button.center_x = button_spec.center[0]
+            button.center_y = button_spec.center[1]
+            self.sprites.append(button)
+
+        # TODO(rob): Also handle images.
