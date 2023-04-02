b2bbe5d3945792fa1687992617ba361b423d9290
Rob <39163+robbrit@users.noreply.github.com>
2023-04-02 15:13:53 -0600

    Support conversations with NPCs. (#53)

--- game/regions/region1.py
+++ game/regions/region1.py
@@ -1,4 +1,5 @@
 from engine import scripts
+from engine.gui import conversation
 
 
 def building1_door(api: scripts.GameAPI) -> None:
@@ -9,6 +10,27 @@ def building1_exit(api: scripts.GameAPI) -> None:
     api.change_region("Region1", "Building 1 Exit")
 
 
-class IntroCharacter(scripts.Script, scripts.SavesAPI):
+class IntroCharacter(scripts.SavesAPI, scripts.Script):
     def on_activate(self, owner: scripts.ScriptOwner, player: scripts.Player) -> None:
-        print("Welcome!")
+        self.api.show_gui(TalkToNPC())
+
+
+class TalkToNPC(conversation.GUI):
+    def __init__(self):
+        # TODO(rob): Add a choice that goes back to the game. In order to do that we
+        # need to start_game() resume the game instead of restarting it.
+        super().__init__(
+            conversation.Conversation(
+                title="Chat with random dude",
+                text="Welcome!",
+                choices=[
+                    conversation.Choice(
+                        text="Hi",
+                        link=conversation.Conversation(
+                            text="Hi!",
+                            choices=[],
+                        ),
+                    )
+                ],
+            )
+        )
