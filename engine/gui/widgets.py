e211f122f5b677bcd4cfb12c58d0654b70352e29
licjon <67080615+licjon@users.noreply.github.com>
2023-04-02 10:16:34 -0400

    Licjon/gui spec (#22)
    * Invalidate duplicate buttons and assets
    
    * Invalidate when button center is off the screen
    
    * ValidationError for file path; refactor
    
    * ValidationError for file path; refactor
    
    * Style changes
    
    * Change button.center to tuple. Refactor duplicate errors.
    
    * Resolve merge conflicts
    
    * Fix imports
    
    * Refactor and check directions and initial select button
    
    * Fix selected_image_asset not in assets error, initial_select_button error

--- engine/gui/widgets.py
+++ engine/gui/widgets.py
@@ -1,3 +1,5 @@
+import collections
+import pathlib
 from typing import (
     Any,
     Dict,
@@ -7,7 +9,7 @@ from typing import (
     Tuple,
 )
 
-from engine import scripts
+from engine import scripts, core
 
 
 class Asset(NamedTuple):
@@ -42,7 +44,7 @@ class Button(NamedTuple):
             right=spec.get("right"),
             up=spec.get("up"),
             down=spec.get("down"),
-            center=spec["center"],
+            center=tuple(spec["center"]),
             action=scripts.load_callable(spec["action"]),
         )
 
@@ -84,15 +86,85 @@ class GUISpec(NamedTuple):
         selected_button_name = spec.get("initial_selected_button")
         selected_button = None
         if selected_button_name is not None:
-            selected_button = next(
-                (b for b in buttons if b.name == selected_button_name),
-                None,
-            )
+            try:
+                selected_button = next(
+                    (b for b in buttons if b.name == selected_button_name)
+                )
+            except StopIteration as exc:
+                raise ValidationError(
+                    f"Initial select button {selected_button_name} does not exist."
+                ) from exc
 
-        return GUISpec(
+        gui_spec = GUISpec(
             assets=[Asset(**a) for a in spec.get("assets", [])],
             buttons=buttons,
             images=[Image(**b) for b in spec.get("images", [])],
             cancel_action=cancel_action,
             initial_selected_button=selected_button,
         )
+
+        gui_spec.validate()
+        return gui_spec
+
+    def validate(self) -> None:
+        """Validates the GUISpec."""
+
+        if not self.buttons and not self.images:
+            raise ValidationError("Must have at least one button or image")
+
+        asset_names = collections.Counter([a.name for a in self.assets])
+        duplicate_assets = [asset for asset, count in asset_names.items() if count > 1]
+        if duplicate_assets:
+            raise ValidationError(f"Duplicate asset name: {duplicate_assets}.")
+
+        button_names = collections.Counter([b.name for b in self.buttons])
+        duplicate_buttons = [name for name, count in button_names.items() if count > 1]
+        if duplicate_buttons:
+            raise ValidationError(f"Duplicate button name: {duplicate_buttons}")
+
+        image_names = collections.Counter([i.image_asset for i in self.images])
+        duplicate_images = [image for image, count in image_names.items() if count > 1]
+        if duplicate_images:
+            raise ValidationError(f"Duplicate image name(s): {duplicate_images}")
+
+        for button in self.buttons:
+            if button.selected_image_asset not in [a.name for a in self.assets]:
+                raise ValidationError(
+                    f"Button {button.selected_image_asset} not in assets"
+                )
+
+        for image in self.images:
+            if image.image_asset not in [i.name for i in self.assets]:
+                raise ValidationError(f"Image {image.image_asset} not in assets")
+
+        for a in self.assets:
+            if not pathlib.Path(a.path).is_file():
+                raise ValidationError(f"Asset path does not exist: {a.path}")
+
+        for button in self.buttons:
+            if button.center[0] < 0 or button.center[0] > core.SCREEN_WIDTH:
+                raise ValidationError(
+                    f"Button {button.name} is off screen horizontally"
+                )
+            if button.center[1] < 0 or button.center[1] > core.SCREEN_HEIGHT:
+                raise ValidationError(f"Button {button.name} is off screen vertically")
+            for direction in ["left", "right", "up", "down"]:
+                name = getattr(button, direction)
+                if name and name not in button_names:
+                    raise ValidationError(
+                        f"Button {name} referenced by {direction} not in buttons"
+                    )
+
+        for image in self.images:
+            if image.center[0] < 0 or image.center[0] > core.SCREEN_WIDTH:
+                raise ValidationError(
+                    f"Image {image.image_asset} is off screen horizontally"
+                )
+            if image.center[1] < 0 or image.center[1] > core.SCREEN_HEIGHT:
+                raise ValidationError(
+                    f"Image {image.image_asset} is off screen vertically"
+                )
+
+
+class ValidationError(Exception):
+    """An error during validation."""
