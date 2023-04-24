f65fd9e4915f82756de81d17922868979d68f29b
Rob <39163+robbrit@users.noreply.github.com>
2023-04-24 09:32:43 -0600

    Added a shopkeeper, added real names. Fixed bug with GUI buttons. (#118)

--- assets/regions/Interior.tsx
+++ assets/regions/Interior.tsx
@@ -1,14 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Interior" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="interior-tileset.png" width="64" height="64"/>
+<tileset version="1.9" tiledversion="1.9.2" name="Interior" tilewidth="32" tileheight="32" tilecount="10" columns="5">
+ <image source="interior-tileset.png" width="160" height="64"/>
  <wangsets>
   <wangset name="Interior" type="mixed" tile="-1">
    <wangcolor name="Wall" color="#ff0000" tile="-1" probability="1"/>
    <wangcolor name="Floor" color="#00ff00" tile="-1" probability="1"/>
    <wangtile tileid="0" wangid="1,1,1,1,1,1,1,1"/>
    <wangtile tileid="1" wangid="2,2,2,2,2,2,2,2"/>
-   <wangtile tileid="2" wangid="1,1,1,1,1,1,1,1"/>
-   <wangtile tileid="3" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="5" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="6" wangid="2,2,2,2,2,2,2,2"/>
   </wangset>
  </wangsets>
 </tileset>
--- game/regions/region1.py
+++ game/regions/region1.py
@@ -5,27 +5,71 @@ from engine import (
 from engine.gui import conversation
 
 
-class IntroCharacter(scripts.SavesAPI, scripts.Script):
+class CoryCotton(scripts.SavesAPI, scripts.Script):
     def on_activate(self, owner: scripts.ScriptOwner, player: scripts.Player) -> None:
-        self.api.show_gui(TalkToNPC())
+        self.api.show_gui(CoryCottonChat())
 
 
-class TalkToNPC(conversation.GUI):
+class LouisMckay(scripts.SavesAPI, scripts.Script):
+    def on_activate(self, owner: scripts.ScriptOwner, player: scripts.Player) -> None:
+        self.api.show_gui(LouisMckayChat())
+
+
+class CoryCottonChat(conversation.GUI):
     def __init__(self):
-        # TODO(rob): Add a choice that goes back to the game. In order to do that we
-        # need to start_game() resume the game instead of restarting it.
+        bye = conversation.Choice(
+            text="Bye!",
+            action=builtin.resume_game,
+        )
+
+        holden = conversation.Choice(
+            text="Holden",
+            link=conversation.Conversation(
+                text=(
+                    "This is a small town named Holden, just a few of us living here now. There's "
+                    "a shop next to me run by my friend Louis, go ahead and run inside if you "
+                    "haven't already."
+                ),
+                choices=[],
+            ),
+        )
+
+        forest = conversation.Choice(
+            text="Forest",
+            link=conversation.Conversation(
+                text=(
+                    "There's a forest to the west. It's dangerous, be sure to pick up some way to "
+                    "defend yourself if you want to go out there."
+                ),
+                choices=[holden, bye],
+            ),
+        )
+        holden.link.choices = [forest, bye]
+
         super().__init__(
             conversation.Conversation(
-                title="Chat with random dude",
-                text="Welcome!",
+                title="Cory Cotton",
+                text=(
+                    "Welcome! My name is Cory, and this town is Holden. You can ask me "
+                    "about anything!"
+                ),
                 choices=[
-                    conversation.Choice(
-                        text="Hi",
-                        link=conversation.Conversation(
-                            text="Hi!",
-                            choices=[],
-                        ),
-                    ),
+                    holden,
+                    forest,
+                    bye,
+                ],
+            )
+        )
+
+
+class LouisMckayChat(conversation.GUI):
+    def __init__(self):
+        super().__init__(
+            conversation.Conversation(
+                title="Louis Mckay",
+                text="Welcome to my shop.",
+                # TODO(rob): Create a "shop" GUI that we can plug in here.
+                choices=[
                     conversation.Choice(
                         text="Bye!",
                         action=builtin.resume_game,
