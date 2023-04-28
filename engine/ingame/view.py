1ca102ebac043e113483da168b622f006a5f4de3
Jon Licjon <licjon@protonmail.com>
2023-04-28 12:11:26 -0400

    Refactor self.width/height to property methods. Comment out setup() because don't know if it is needed.

--- engine/ingame/view.py
+++ engine/ingame/view.py
@@ -17,8 +17,6 @@ class InGameView:
     Renders the world while the user is in the game.
     """
 
-    width: int
-    height: int
     game_world: world.World
     camera: arcade.Camera
     gui: Optional[scripts.GUI]
@@ -30,8 +28,6 @@ class InGameView:
         viewport_size: Tuple[int, int],
         gui: Optional[scripts.GUI],
     ):
-        self.width = 0
-        self.height = 0
         self.game_world = game_world
         self.gui = gui
         self.camera_position = (0, 0)
@@ -44,10 +40,16 @@ class InGameView:
             viewport_size[1],
         )
 
-    def setup(self) -> None:
-        """Sets up the view."""
-        self.width = self.game_world.width * self.game_world.tile_width
-        self.height = self.game_world.height * self.game_world.tile_height
+    @property
+    def world_width(self) -> int:
+        return self.game_world.width * self.game_world.tile_width
+
+    @property
+    def world_height(self) -> int:
+        return self.game_world.height * self.game_world.tile_height
+
+#    def setup(self) -> None:
+#        """Sets up the view."""
 
     def on_draw(self) -> None:
         """Renders the view."""
@@ -61,8 +63,6 @@ class InGameView:
     def on_update(self, delta_time: float) -> None:
         """Triggers an update for the view."""
         # pylint: disable=unused-argument
-        if self.camera.viewport_width < self. or self.camera.viewport_height < self.window_size[1]:
-            self.on_resize()
         self._center_camera_to_player()
 
     def to_world_coords(self, screen_x: int, screen_y: int) -> Tuple[int, int]:
@@ -79,14 +79,14 @@ class InGameView:
         v_height = self.camera.viewport_height
 
         screen_center_x = min(
-            self.width - v_width,
+            self.world_width - v_width,
             max(
                 0,
                 player.center_x - v_width / 2,
             ),
         )
         screen_center_y = min(
-            self.height - v_height,
+            self.world_height - v_height,
             max(
                 0,
                 player.center_y - v_height / 2,
@@ -96,19 +96,3 @@ class InGameView:
         player_centered = screen_center_x, screen_center_y
 
         self.camera.move_to(player_centered)
-
-    def on_resize(self) -> None:
-        """self.camera_pos is used to store the position of the camera.
-        When the window is resized, on_resize is called and the position of
-        the camera is adjusted so that it is centered in the screen.
-        The arcade.set_viewport method is then called with the adjusted
-        camera position and the new screen size to update the viewport."""
-        if self.camera.viewport_width < arcade.Window.get_size()[0]:
-            self.camera_position[0] = (arcade.Window.get_size()[0] - self.width) / 2
-        if self.camera.viewport_height < arcade.Window.get_size()[1]:
-            self.camera_position[1] = (arcade.Window.get_size()[1] - self.height) / 2
-
-        arcade.set_viewport(
-            self.camera_position[0], self.camera.viewport_width + self.camera_position[0],
-            self.camera_position[1], self.camera.viewport_height + self.camera_position[1],
-        )
