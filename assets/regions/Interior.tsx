03aac4706cae8ff1ab23a95aab2d66af8ad4f2d8
Rob Britton <rob@robbritton.com>
2023-05-02 10:04:52 -0400

    Added upstairs to shop.

--- assets/regions/Interior.tsx
+++ assets/regions/Interior.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Interior" tilewidth="32" tileheight="32" tilecount="10" columns="5">
- <image source="interior-tileset.png" width="160" height="64"/>
+<tileset version="1.9" tiledversion="1.9.2" name="Interior" tilewidth="32" tileheight="32" tilecount="25" columns="5">
+ <image source="interior-tileset.png" width="160" height="160"/>
  <wangsets>
   <wangset name="Interior" type="mixed" tile="-1">
    <wangcolor name="Wall" color="#ff0000" tile="-1" probability="1"/>
