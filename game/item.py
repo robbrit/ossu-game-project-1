90d60db900b634b46ecbc9d533973bcb7f36f0ec
Exkywor <fernandoquant.dev@gmail.com>
2023-05-06 16:05:55 -0600

    Add name property to Item

--- game/item.py
+++ game/item.py
@@ -13,7 +13,8 @@ class Item:
     """Protocol to define an Item."""
 
     _asset: str
+    _name: str
     item_type: ItemType
 
-    def __init__(self, asset, item_type):
+    def __init__(self, name, asset, item_type):
         """Initialize the Item with an asset and a type"""
