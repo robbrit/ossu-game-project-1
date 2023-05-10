197c7f4ba63e85a317d098265e51bba1cbdd8270
Rob <39163+robbrit@users.noreply.github.com>
2023-05-10 08:10:38 -0600

    Made `create_sprite` raise on name conflict. (#159)
    * Made `create_sprite` raise on name conflict.
    
    * Lint.

---
+++ engine/model/test/test_world.py
@@ -0,0 +1,48 @@
+import unittest
+from unittest import mock
+
+import arcade
+
+from engine.model import world
+from engine.test import factories
+
+
+class CreateSpriteTest(unittest.TestCase):
+    @mock.patch("engine.model.game_sprite.GameSprite")
+    @mock.patch("arcade.load_tilemap")
+    @mock.patch("engine.model.player_sprite.PlayerSprite")
+    def test_cannot_add_existing_sprite(
+        self,
+        mocked_player,
+        mocked_tilemap,
+        mocked_game_sprite,
+    ):
+        # TODO(rob): Move all the mocking for Arcade into its own decorator.
+        mock_tilemap = mock.Mock()
+        mock_tilemap.object_lists = {
+            "Key Points": [
+                arcade.TiledObject(
+                    name="Start",
+                    shape=[0, 0],
+                ),
+            ],
+        }
+        mock_tilemap.sprite_lists = {
+            "Wall Tiles": arcade.SpriteList(),
+        }
+        mock_tilemap.width = 10
+        mock_tilemap.height = 10
+        mock_tilemap.tile_width = 10
+        mock_tilemap.tile_height = 10
+        mocked_tilemap.return_value = mock_tilemap
+
+        api = mock.Mock()
+        sprite_spec = factories.fake_sprite_spec()
+        spec = factories.fake_game_spec()
+
+        w = world.World(api, spec, initial_player_data={})
+
+        w.create_sprite(sprite_spec, "sprite", (0, 0), script=mock.Mock())
+
+        with self.assertRaises(world.SpriteAlreadyExists):
+            w.create_sprite(sprite_spec, "sprite", (0, 0), script=mock.Mock())
---
+++ engine/test/factories.py
@@ -0,0 +1,68 @@
+"""This module defines a set of factory functions to generate data for tests."""
+
+import dataclasses
+
+from engine import spec
+
+
+def fake_region_spec(**args) -> spec.RegionSpec:
+    """Constructs a fake RegionSpec."""
+    defaults = {
+        "tiled_mapfile": "",
+    }
+    defaults.update(args)
+    return spec.RegionSpec(**defaults)
+
+
+def fake_world_spec(**args) -> spec.WorldSpec:
+    """Constructs a fake WorldSpec."""
+    defaults = {
+        "regions": {
+            "region1": fake_region_spec(),
+        },
+        "initial_region": "region1",
+    }
+    defaults.update(args)
+    return spec.WorldSpec(**defaults)
+
+
+def fake_animation_spec(**args) -> spec.AnimationSpec:
+    """Constructs a fake AnimationSpec."""
+    defaults = {
+        "num_frames": 1,
+        "frame_speed": 1.0,
+    }
+    defaults.update(args)
+    return spec.AnimationSpec(**defaults)
+
+
+def fake_sprite_spec(**args) -> spec.GameSpriteSpec:
+    """Constructs a fake GameSpriteSpec."""
+    defaults = {
+        "width": 10,
+        "height": 10,
+        "initial_animation": "idle",
+        "root_directory": "",
+        "animations": {
+            "idle": dataclasses.asdict(fake_animation_spec()),
+        },
+    }
+    defaults.update(**args)
+    return spec.GameSpriteSpec(**defaults)
+
+
+def fake_game_spec(**args) -> spec.GameSpec:
+    """Constructs a fake GameSpec."""
+    defaults = {
+        "world": dataclasses.asdict(fake_world_spec(**args.get("world", {}))),
+        "player_spec": dataclasses.asdict(fake_sprite_spec(**args.get("player", {}))),
+        "sprites": {},
+        "guis": {},
+    }
+
+    for key in ["world", "player_spec"]:
+        if key in args:
+            del args[key]
+
+    defaults.update(args)
+    return spec.GameSpec(**defaults)
