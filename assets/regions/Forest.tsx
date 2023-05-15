07deafa49fc94891b070a524884887766d8ba88a
Rob Britton <rob@robbritton.com>
2023-05-15 10:25:38 -0400

    Reworked the forest tilesets a bit.

--- assets/regions/Forest.tsx
+++ assets/regions/Forest.tsx
@@ -4,42 +4,30 @@
  <wangsets>
   <wangset name="Forest" type="mixed" tile="-1">
    <wangcolor name="Grass" color="#00ff11" tile="-1" probability="1"/>
-   <wangcolor name="Tree" color="#007900" tile="-1" probability="1"/>
    <wangcolor name="Dirt" color="#550000" tile="-1" probability="1"/>
-   <wangcolor name="Wall" color="#6c6c6c" tile="-1" probability="1"/>
+   <wangcolor name="Forest" color="#005500" tile="-1" probability="1"/>
    <wangtile tileid="0" wangid="1,1,1,1,1,1,1,1"/>
-   <wangtile tileid="1" wangid="2,2,2,2,2,2,2,2"/>
-   <wangtile tileid="2" wangid="3,3,3,3,3,3,3,3"/>
-   <wangtile tileid="3" wangid="4,4,4,4,4,4,4,4"/>
+   <wangtile tileid="1" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,2,0,0,0"/>
+   <wangtile tileid="3" wangid="0,0,0,0,0,2,0,0"/>
    <wangtile tileid="8" wangid="1,1,1,1,1,1,1,1"/>
-   <wangtile tileid="9" wangid="2,2,2,2,2,2,2,2"/>
-   <wangtile tileid="10" wangid="3,3,3,3,3,3,3,3"/>
-   <wangtile tileid="11" wangid="4,4,4,4,4,4,4,4"/>
+   <wangtile tileid="9" wangid="0,0,2,0,0,0,0,0"/>
+   <wangtile tileid="10" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="11" wangid="0,0,0,0,0,0,2,0"/>
    <wangtile tileid="16" wangid="1,1,1,1,1,1,1,1"/>
-   <wangtile tileid="17" wangid="2,2,2,2,2,2,2,2"/>
-   <wangtile tileid="18" wangid="3,3,3,3,3,3,3,3"/>
-   <wangtile tileid="19" wangid="4,4,4,4,4,4,4,4"/>
+   <wangtile tileid="17" wangid="0,2,0,0,0,0,0,0"/>
+   <wangtile tileid="18" wangid="2,0,0,0,0,0,0,0"/>
+   <wangtile tileid="19" wangid="0,0,0,0,0,0,0,2"/>
    <wangtile tileid="24" wangid="1,1,1,1,1,1,1,1"/>
-   <wangtile tileid="25" wangid="2,2,2,2,2,2,2,2"/>
-   <wangtile tileid="26" wangid="3,3,3,3,3,3,3,3"/>
-   <wangtile tileid="27" wangid="4,4,4,4,4,4,4,4"/>
-  </wangset>
-  <wangset name="Forest Corners" type="corner" tile="-1">
-   <wangcolor name="Grass" color="#ff0000" tile="-1" probability="1"/>
-   <wangcolor name="Tree" color="#00ff00" tile="-1" probability="1"/>
-   <wangcolor name="Dirt" color="#0000ff" tile="-1" probability="1"/>
-   <wangtile tileid="32" wangid="0,0,0,0,0,0,0,1"/>
-   <wangtile tileid="33" wangid="0,0,0,0,0,0,0,2"/>
-   <wangtile tileid="34" wangid="0,0,0,0,0,0,0,3"/>
-   <wangtile tileid="40" wangid="0,0,0,0,0,1,0,0"/>
-   <wangtile tileid="41" wangid="0,0,0,0,0,2,0,0"/>
-   <wangtile tileid="42" wangid="0,0,0,0,0,3,0,0"/>
-   <wangtile tileid="48" wangid="0,1,0,0,0,0,0,0"/>
-   <wangtile tileid="49" wangid="0,2,0,0,0,0,0,0"/>
-   <wangtile tileid="50" wangid="0,3,0,0,0,0,0,0"/>
-   <wangtile tileid="56" wangid="0,0,0,1,0,0,0,0"/>
-   <wangtile tileid="57" wangid="0,0,0,2,0,0,0,0"/>
-   <wangtile tileid="58" wangid="0,0,0,3,0,0,0,0"/>
+   <wangtile tileid="25" wangid="0,0,0,3,0,0,0,0"/>
+   <wangtile tileid="26" wangid="0,0,0,0,3,0,0,0"/>
+   <wangtile tileid="27" wangid="0,0,0,0,0,3,0,0"/>
+   <wangtile tileid="33" wangid="0,0,3,0,0,0,0,0"/>
+   <wangtile tileid="34" wangid="3,3,3,3,3,3,3,3"/>
+   <wangtile tileid="35" wangid="0,0,0,0,0,0,3,0"/>
+   <wangtile tileid="41" wangid="0,3,0,0,0,0,0,0"/>
+   <wangtile tileid="42" wangid="3,0,0,0,0,0,0,0"/>
+   <wangtile tileid="43" wangid="0,0,0,0,0,0,0,3"/>
   </wangset>
  </wangsets>
 </tileset>
