ac494db22c37c1d9b605f83e59389dc8b740123c
Rob Britton <rob@robbritton.com>
2023-05-16 10:47:27 -0400

    Added support for dynamic conversations.

--- game/regions/region1.py
+++ game/regions/region1.py
@@ -27,7 +27,7 @@ class CoryCottonChat(conversation.GUI):
     def __init__(self):
         holden = conversation.Choice(
             text="Holden",
-            link=conversation.Conversation(
+            link=conversation.StaticConversation(
                 text=(
                     "This is a small town named Holden, just a few of us living here now. There's "
                     "a shop next to me run by my friend Louis, go ahead and run inside if you "
@@ -39,7 +39,7 @@ class CoryCottonChat(conversation.GUI):
 
         forest = conversation.Choice(
             text="Forest",
-            link=conversation.Conversation(
+            link=conversation.StaticConversation(
                 text=(
                     "There's a forest to the west. It's dangerous, be sure to pick up some way to "
                     "defend yourself if you want to go out there."
@@ -50,7 +50,7 @@ class CoryCottonChat(conversation.GUI):
         holden.link.choices = [forest]
 
         super().__init__(
-            conversation.Conversation(
+            conversation.StaticConversation(
                 title="Cory Cotton",
                 text=(
                     "Welcome! My name is Cory, and this town is Holden. You can ask me "
@@ -68,14 +68,14 @@ class AdeleCottonChat(conversation.GUI):
     def __init__(self):
         cory = conversation.Choice(
             text="Cory",
-            link=conversation.Conversation(
+            link=conversation.StaticConversation(
                 text="That's my husband. He's standing over there.",
                 choices=[],
             ),
         )
 
         super().__init__(
-            conversation.Conversation(
+            conversation.StaticConversation(
                 title="Adele Cotton",
                 text=("Hello, my name is Adele. I live here with my husband Cory."),
                 choices=[
@@ -88,7 +88,7 @@ class AdeleCottonChat(conversation.GUI):
 class LouisMckayChat(conversation.GUI):
     def __init__(self):
         super().__init__(
-            conversation.Conversation(
+            conversation.StaticConversation(
                 title="Louis Mckay",
                 text="Welcome to my shop.",
                 choices=[],
